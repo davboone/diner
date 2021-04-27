fed7ce1f6e0b4fa96af4fc683207ba33df371031
David Boone <Dboone2@mail.greenriver.edu>
2021-04-27 12:27:34 -0400

    Added new routes, added .htaccess file

--- index.php
+++ index.php
@@ -20,5 +20,12 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 
+$f3->route('GET /breakfast', function(){
+
+    //Display the home page
+    $view = new Template();
+    echo $view->render('views/breakfast.html');
+});
+
 //Run Fat-Free
 $f3->run();
