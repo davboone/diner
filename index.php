9db4febacfc03ce451a5b169843fde6084a98ba7
David Boone <Dboone2@mail.greenriver.edu>
2021-04-28 23:46:50 -0400

    Moved images folder because of .htaccess file

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+//start session
+session_start();
+
 //This is my controller for the diner project
 
 //Turn on error-reporting
@@ -32,6 +35,40 @@ $f3->route('GET /lunch', function(){
     //Display the home page
     $view = new Template();
     echo $view->render('views/lunch.html');
+});
+
+$f3->route('GET|POST /order1', function(){
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_SESSION['food'] = $_POST['food'];
+        $_SESSION['meal'] = $_POST['meal'];
+        header('location: order2');
+    }
+
+    //Display the home page
+    $view = new Template();
+    echo $view->render('views/Orderform1.html');
+});
+
+$f3->route('GET|POST /order2', function(){
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        //Data validation will go here
+
+        $_SESSION['conds'] = implode(", ",$_POST['conds']);
+        header('location: summary');
+    }
+
+    //Display the home page
+    $view = new Template();
+    echo $view->render('views/Orderform2.html');
+});
+
+$f3->route('GET /summary', function(){
+
+    //Display the home page
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 //Run Fat-Free
